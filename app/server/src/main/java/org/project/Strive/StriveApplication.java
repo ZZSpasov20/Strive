d474741b9dcfa87de6ca84c707d94c476213455c
SABoev20 <SABoev20@codingburgas.bg>
2024-03-19 01:00:39 +0200

    Set up spring initializr

---
+++ app/server/src/main/java/org/project/Strive/StriveApplication.java
@@ -0,0 +1,13 @@
+package org.project.Strive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriveApplication.class, args);
+	}
+
+}
