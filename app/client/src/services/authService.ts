45990ca0cb1e22fa87c24c43d6b9c9e32f5b6491
SABoev20 <SABoev20@codingburgas.bg>
2024-04-19 23:43:00 +0300

    Add basic authService

--- app/client/src/services/authService.ts
+++ app/client/src/services/authService.ts
@@ -1,2 +1,34 @@
 import axios from 'axios';
 import { User } from '../models/User';
+
+export class UserService {
+    private API_URL = 'localhost:8080/';
+
+    public async login(username: string, password: string){
+        try {
+            const response = await axios.post(`${this.API_URL}login`,
+                {
+                    username,
+                    password
+                }
+            );
+            return response.data;
+        } catch (error) {
+            throw new Error('Failed to login user');
+        }
+    }
+
+    public async signup(username: string, password: string){
+        try {
+            const response = await axios.post(`${this.API_URL}signup`,
+                {
+                    username,
+                    password
+                }
+            );
+            return response.data;
+        } catch (error) {
+            throw new Error('Failed to signup user');
+        }
+    }
+}
